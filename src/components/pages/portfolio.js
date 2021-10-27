e87024cfd8a7e1f3a0a0af2277048f0a4e17f340
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-27 13:45:15 -0700

    added project component

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -1,9 +1,12 @@
 
 import React from 'react';
+import Project from './project'
+
 export default function Portfolio() {
     return (
         <div>
             <h1>Portfolio</h1>
+            <Project />
         </div>
     );
 }
