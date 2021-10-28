e2219f0b694b9eab4ebb81428c5e3aa29096f9d0
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-28 09:56:29 -0700

    updated styling

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,21 +1,24 @@
-import * as React from 'react';
-import ResumePic from "../assets/resume.png"
-import Container from '@mui/material/Container';
+import * as React from "react";
+import ResumePic from "../assets/resume.png";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
 
 export default function Resume() {
   return (
     <React.Fragment>
-    <Container fixed>
-    <div>
+      <Container fixed>
         <div>
+          <Link
+            href="https://drive.google.com/file/d/1mCQOajsW_QIlv_T33E0Y4IR_gRp2xW00/view?usp=sharing"
+            underline="none"
+          >
+            Download Here
+          </Link>
+          <div>
             <img alt="resume" src={ResumePic} />
+          </div>
         </div>
-    <div>
-        <a alt="resume" href="https://drive.google.com/file/d/1mCQOajsW_QIlv_T33E0Y4IR_gRp2xW00/view?usp=sharing">Download Here</a>
-    </div>
-    </div>
-    </Container>
-  </React.Fragment>
+      </Container>
+    </React.Fragment>
   );
 }
-
