081b0014142e1bbe355424eb8ccae092208d7fd5
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-27 17:11:22 -0700

    added footer styling and instagram link

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,10 +1,22 @@
 
 import React from 'react';
+import Avatar from '../assets/avatar.png'
 
 export default function About() {
     return (
+      <section className="container">
+      <h2 class="top-title">Mackenzie Saideman</h2>
+      <hr></hr>
       <div>
-        <h1>About Me</h1>
+          <img id="avatar" src={Avatar} alt="Mackenzie Saideman" />
+
+          <p>
+          Project Manager with a demonstrated history of working in the life sciences, medical device, and nonprofit industries. 
+          </p>
+          <p>
+          Skilled in data analysis, coordination of complex projects, process improvement in highly regulated industries, and development of E-commerce websites.
+          </p>
       </div>
+  </section>
     );
   }
